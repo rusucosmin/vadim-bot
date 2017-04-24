aa7653ad20b7b8bc64c9858d6981ab1bca3e3c7e
Cosmin <cr.rusucosmin@gmail.com>
2017-04-24 23:22:26 +0300

    added greetings

--- server.js
+++ server.js
@@ -22,3 +22,59 @@ app.get("/webhook", function(req, res) {
     res.sendStatus(403);
   }
 });
+
+app.post("/webhook", function(req, res) {
+  if(req.body.object === "page") {
+    req.body.entry.forEach(function(entry) {
+      entry.messaging.forEach(function(event) {
+        if(event.postback) {
+          processPostBack(event);
+        }
+     });
+    });
+    res.sendStatus(200);
+  }
+});
+
+function processPostback(event) {
+  var senderId = event.sender.id;
+  var payload = event.postback.payload;
+
+  if(payload === "Greeting") {
+    request({
+      url: "https://graph.facebook.com/v2.6/" + senderId,
+      qs: {
+        access_token: process.env.PAGE_ACCESS_TOKEN,
+        fields: "first_name"
+      },
+      method: "GET"
+    }, function(error, response, body) {
+      var greeting = "";
+      if(error) {
+        console.log("Error getting user's name: " + error);
+      } else {
+        var bodyObj = JSON.parse(body);
+        name = bodyObj.first_name;
+        greeting = "Salut " + name + ". ";
+      }
+      var message = greeting + "Politica e arta de a sta la pândă.Politica e arta de a sta la pândă.";
+      sendMessage(senderId, {text: message});
+    });
+  }
+}
+
+function sendMessage(recipientId, message) {
+  request({
+    url: "https://graph.facebook/com/v2.6/me/messages",
+    qs: {access_token: process.env.PAGE_ACCESS_TOKEN},
+    method: "POST",
+    json: {
+      recipient: {id: recipientId},
+      message: message
+    }
+  }, function(error, response, body) {
+    if(error) {
+      console.log("Error sendong message: " + response.error);
+    }
+  });
+}
