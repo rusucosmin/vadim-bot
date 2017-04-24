2518245d8990c9092e43a2422560f5a7d12bb74b
Cosmin <cr.rusucosmin@gmail.com>
2017-04-24 21:52:26 +0300

    Initial commit

---
+++ index.js
@@ -0,0 +1,24 @@
+var express = require("express");
+var request = require("request");
+var bodyParser = require("body-parser");
+
+var app = express();
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
+
+app.listen((preocess.env.PORT || 5000));
+
+app.get("/", function(req, rest) {
+  res.send("Deployed!");
+});
+
+
+app.get("/webhook", function(req, res) {
+  if(req.query["hub.verify_toekn"] == "this_is_my_token") {
+    console.log("Verified webhook");
+    res.status(200).send(req.query["hub.challenge"]);
+  } else {
+    console.error("Verification failed. The tokens do not mathc");
+    res.sendStatus(403);
+  }
+});
