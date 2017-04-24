e57c743d885445fbaaa26bfd4d964a9e506d1247
Cosmin <cr.rusucosmin@gmail.com>
2017-04-24 22:16:36 +0300

    fixed typo

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ app.get("/", function(req, res) {
 
 
 app.get("/webhook", function(req, res) {
-  if(req.query["hub.verify_toekn"] == "this_is_my_token") {
+  if(req.query["hub.verify_token"] == process.env.VERIFICATION_TOKEN) {
     console.log("Verified webhook");
     res.status(200).send(req.query["hub.challenge"]);
   } else {
